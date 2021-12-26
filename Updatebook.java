dcb1bf749b3f628f9923ffdffc9feeba8df4f192
Xxuao <2631509949@qq.com>
2021-12-27 00:02:26 +0800

    my first commit

---
+++ Updatebook.java
@@ -0,0 +1,81 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import com.google.gson.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import com.google.gson.reflect.TypeToken;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet(urlPatterns = "/Updatebook")
+public class Updatebook extends HttpServlet {
+   final static String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+   final static String URL = "jdbc:mysql://116.62.188.197/linuxExam";
+   final static String USER = "root";
+   final static String PASS = "Xuao20001224!";
+   final static String SQL_UPDATE_BOOK = "UPDATE t_Book SET name= ? , author = ? WHERE id=?";
+
+   public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      response.setContentType("application/json");
+      response.setCharacterEncoding("UTF-8");
+
+      Book req = getRequestBody(request);
+      PrintWriter out = response.getWriter();
+
+      out.println(updateBook(req));
+      out.flush();
+      out.close();
+   }
+
+   private Book getRequestBody(HttpServletRequest request) throws IOException {
+      Book note = new Book();
+      StringBuffer bodyJ = new StringBuffer();
+      String line = null;
+      BufferedReader reader = request.getReader();
+      while ((line = reader.readLine()) != null)
+         bodyJ.append(line);
+    Gson gson = new Gson();
+note = gson.fromJson(bodyJ.toString(), new TypeToken<Book>() {
+      }.getType());
+      return note;
+   }
+
+   private int updateBook(Book req) {
+      Connection conn = null;
+      PreparedStatement stmt = null;
+      int retcode = -1;
+      try {
+         Class.forName(JDBC_DRIVER);
+         conn = DriverManager.getConnection(URL, USER, PASS);
+         stmt = conn.prepareStatement(SQL_UPDATE_BOOK);
+
+         stmt.setString(1, req.name);
+         stmt.setString(2, req.author);
+         stmt.setInt(3, req.id);
+
+         int row = stmt.executeUpdate();
+         if (row > 0)
+            retcode = 1;
+
+         stmt.close();
+         conn.close();
+      } catch (SQLException se) {
+         se.printStackTrace();
+      } catch (Exception e) {
+         e.printStackTrace();
+      } finally {
+         try {
+            if (stmt != null)
+               stmt.close();
+            if (conn != null)
+               conn.close();
+         } catch (SQLException se) {
+            se.printStackTrace();
+         }
+      }
+     return retcode;
+   }
+}
+      
